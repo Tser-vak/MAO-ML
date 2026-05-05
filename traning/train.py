690f39cc2b44a951607b7a459d23c28f482b3894
Maxim <maxitservak@gmail.com>
2026-05-05 21:00:39 +0300

    Created Visual ,First phase of train.py Visual have the tools for visualization of results Trainning first phase Data handeling

---
+++ traning/train.py
@@ -0,0 +1,35 @@
+import mlflow
+import pandas as pd
+import numpy as np
+from imblearn.pipeline import Pipeline as ImbPipeline
+from sklearn.pipeline import Pipeline as SklearnPipeline
+from sklearn.model_selection import StratifiedKFold, cross_validate
+from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+from sklearn.metrics import (
+    f1_score, roc_auc_score, balanced_accuracy_score, 
+    precision_score, recall_score, log_loss, confusion_matrix
+)
+
+# Load OPP functions
+from data.data import DataProcessor,DataConv # Data handeling processes
+from classifiers.clf import ModelFactory # Budle of Classifier and hyper-parameter tuning 
+from descriptors_handeling.feature_hand import FearuteSelector  # Feature selection (RFE) object
+from visualization.visual import ModelVisualizer # Budle of Visualizations tools
+
+def main():
+    print('Phase 1 Laoding and Cleaning Data\n')
+    file = r'C:\Users\Max\Desktop\MAO-ML\traning\data\MAOB_PD_Activity_with_descriptors.csv'
+    cleaner = DataProcessor(file_path=file,ignore_desc = ["Molecule ChEMBL ID","Smiles","Standard Type","Standard Relation","Standard Value","Standard Units","Label"],
+         labels = "Label")
+    # Load and Clean Data (Info data.py)
+    cleaner.load_data()
+
+    # Split Data
+    (X_train, X_test, y_train, y_test) = cleaner.get_split()
+    
+
+
+if __name__ == "__main__" : 
+    main()
